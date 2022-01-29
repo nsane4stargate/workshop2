eb1d80b8bb6d73e770ee7d6e84217a6276cbc1ba
Lea <nsane4stargate@gmail.com>
2022-01-28 18:45:34 -0700

    changed to urlpatterns =[path]+static()

--- app/nc_tutorials/urls.py
+++ app/nc_tutorials/urls.py
@@ -21,6 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tutorials.urls')),
-    path('', include('users.urls'))
-    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-]
+    path('', include('users.urls')) 
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
